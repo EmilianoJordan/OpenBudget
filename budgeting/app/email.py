1bd87b3897c13b822766fd67bc981198df691a3d
Emiliano Jordan <emiliano.jordan@gmail.com>
2019-02-15 12:55:14 -0700

    Adding Email Functionality

---
+++ budgeting/app/email.py
@@ -0,0 +1,25 @@
+"""
+Created: 2/15/2019
+Author: Emiliano Jordan,
+        https://github.com/EmilianoJordan
+        https://www.linkedin.com/in/emilianojordan/,
+        Most other things I'm @emilianojordan
+"""
+from flask_mail import Message
+
+from . import mail
+from .decorators import thread
+from ...ob import app
+
+
+@thread
+def send_async_email(current_app, msg):
+    with current_app.app_context():
+        mail.send(msg)
+
+
+def send_email(subject, sender, recipients, text_body, html_body):
+    msg = Message(subject, sender=sender, recipients=recipients)
+    msg.body = text_body
+    msg.html = html_body
+    send_async_email(app, msg)
