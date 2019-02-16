9822808631a76bc36be827660e8bc1eacc1d9ae9
Emiliano Jordan <emiliano.jordan@gmail.com>
2019-02-16 13:16:15 -0700

    Reset

--- budgeting/app/api/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Created: 1/22/2019
-Author: Emiliano Jordan,
-        https://github.com/EmilianoJordan
-        https://www.linkedin.com/in/emilianojordan/,
-        Most other things I'm @emilianojordan
-"""
--- tests/integration/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Created: 2/7/2019
-Author: Emiliano Jordan,
-        https://github.com/EmilianoJordan
-        https://www.linkedin.com/in/emilianojordan/,
-        Most other things I'm @emilianojordan
-"""
